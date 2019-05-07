d01e481401ddb578e9ff5d7f7babe06e06dace00
antonyvigouret <44910926+antonyvigouret@users.noreply.github.com>
2019-05-07 15:09:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,24 @@
 
 int main(void){
     noeud_t *arbre = NULL;
-    char *mot = NULL;
+    char *mot;
 
     FILE *fic;
     fic = fopen("dico.txt", "r");
     if (fic == NULL) {
-        printf("erreur ouverture du fichier\n");
+        printf("Erreur ouverture du fichier\n");
     }else {
-        while (fgets(mot, MAX, fic)) {
-            if (mot[strlen(mot)-1] == '\n') mot[strlen(mot)-1] = '\0'; 
-            printf("%s\n", mot);
-            insertion(mot, &arbre);
-            afficherArbre(arbre);                                                                      
-        }                                                      
+        fgets(mot, MAX, fic);
+        //insertion(mot, &arbre); 
+        // while (fgets(mot, MAX, fic)) {
+        //     if (mot[strlen(mot)-1] == '\n') {
+        //         mot[strlen(mot)-1] = '\0'; 
+        //     }
+        //     printf("%s\n", mot);
+        //     insertion(mot, &arbre);                                                                    
+        // }                                                    
         fclose(fic);
     }
+    afficherArbre(arbre);
     return 0;
 }
